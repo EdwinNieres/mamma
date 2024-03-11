29650be4bfada00f61aa0e8efe836285ce1397ba
Edwin Nieres <ceosbtc@gmail.com>
2024-03-11 08:24:21 -0300

    añadiendo los archivos

---
+++ src/components/HeaderApp.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Banner from "../assets/banner.png";
+
+const HeaderApp = () => {
+  return (
+    <>
+      <div>
+        <img src={Banner} className="img-banner"></img>
+      </div>
+    </>
+  );
+};
+
+export default HeaderApp;
